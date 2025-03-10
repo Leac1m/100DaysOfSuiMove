16f0cc5f3189336ef9903b99616572ebfc9d232f
leac1m <obemichael10@gmail.com>
2025-03-10 22:21:12 +0100

    worked on lottery info cards

--- lottery/lottery-frontend/src/component/LotteryCard.tsx
+++ lottery/lottery-frontend/src/component/LotteryCard.tsx
@@ -1,33 +1,20 @@
 function LotteryCard() {
     return (
       <div 
-        className="bg-primary flex flex-col  rounded-[12px] p-[12px] gap-[12px]"
+        className="bg-primary flex flex-row justify-between rounded-[12px] p-[12px] gap-[12px] text-text-white font-semibold"
       >
-        <div className="flex flex-row justify-between">
-          <h3 className="h-[30px] text-[16px] text-secondary font-semibold">you won!</h3>
-          <div className="bg-ligth-blue rounded-[8px] px-[12px] py-[4px]">Today</div>
-        </div>
+        <div className="flex flex-col gap-[20px]">
+          <div className="flex flex-col gap-[12px]">
+            <div className="text-2xl">Lucky</div>
+            <div className="text-[16px]">1000 SUI</div>
+          </div>
 
-        <div className="flex flex-row justify-between text-text-white">
-          <h3 className="text-[24px] font-bold">UroMillions</h3>
-          <div className="bg-secondary px-[12px] py-[4px] rounded-[8px]">claim</div>
+          <div className="flex flex-row gap-[20px]">
+            <div className="bg-ligth-blue text-text-green rounded-[10px] px-[12px] py-[4px]">Today</div>
+            <div className="bg-ligth-blue text-text-green rounded-[10px] px-[12px] py-[4px]">6:00pm</div>
+          </div>
         </div>
-
-        <div className="flex flex-row justify-between text-text-white font-semibold">
-          <h3 className="">Number</h3>
-          <div className="">83</div>
-        </div>
-
-
-        <div className="flex flex-row justify-between text-text-white font-semibold">
-          <h3 className="">Your prize</h3>
-          <div className="">225 SUI</div>
-        </div>
-
-        <div className="flex flex-row justify-between text-text-white font-semibold">
-          <h3 className="">Pool prize</h3>
-          <div className="">1000 SUI</div>
-        </div>
+        <div className="bg-secondary px-4 h-8 py-1 rounded-[10px]">play</div>
       </div>
     )
   }
